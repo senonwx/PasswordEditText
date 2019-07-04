eb036a3f4bc3ef7e40152dd4f7c452a6f93470f4
senonwx <1083911695@qq.com>
2019-07-04 15:29:37 +0800

    啦啦啦

---
+++ app/src/main/java/com/senon/passwordedittext/MainActivity.java
@@ -0,0 +1,125 @@
+package com.senon.passwordedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    private PasswordEditText editText,editText2,editText3;
+    private Button clear_text,clear,set,get_password;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editText3);
+        clear_text = findViewById(R.id.clear_text);
+        clear = findViewById(R.id.clear);
+        set = findViewById(R.id.set);
+        get_password = findViewById(R.id.get_password);
+        tv = findViewById(R.id.tv);
+
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        //清空输入框
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.clearText();
+                editText2.clearText();
+                editText3.clearText();
+            }
+        });
+
+        //是否明文显示
+        clear_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                editText.setShowPwd(!editText.getShowPwd());
+                editText2.setShowPwd(!editText2.getShowPwd());
+                editText3.setShowPwd(!editText3.getShowPwd());
+        }
+        });
+
+        //设置数字
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setPwdText("1234");
+                editText2.setPwdText("1234");
+                editText3.setPwdText("123456");
+            }
+        });
+
+        //设置数字
+        get_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("editText:"+editText.getPwdText()+
+                        "\neditText2:"+editText2.getPwdText()+
+                        "\neditText3:"+editText3.getPwdText());
+            }
+        });
+    }
+
+    private void initView() {
+        //设置密码框style
+        editText.initStyle(R.drawable.background_box, 4, 15,
+                0x00ffffff,0xff333333, 20,
+                R.drawable.backgroud_password);
+        editText.setOnTextFinishListener(new PasswordEditText.OnTextFinishListener() {
+            @Override
+            public void onFinish(String str) {
+                Log.e(TAG,"onFinish  str= " + str);
+                editText2.setFocus();
+            }
+        });
+
+
+        //设置密码框style
+        editText2.initStyle(R.drawable.background_box, 4, 15,
+                0x00ffffff,0xff333333, 20,
+                R.drawable.backgroud_password);
+        editText2.setOnTextFinishListener(new PasswordEditText.OnTextFinishListener() {
+            @Override
+            public void onFinish(String str) {
+                Log.e(TAG,"onFinish  str= " + str);
+                if(str.equals(editText.getPwdText())){
+                    Toast.makeText(MainActivity.this,"输入密码相同，恭喜啊！"+str,Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"两次密码不同，走开啊！",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //设置密码框style
+        editText3.initStyle(R.drawable.background_box_stroke, 6, 1,
+                0xff999999 ,0xff333333, 20,
+                R.drawable.backgroud_password_tran);
+        editText3.setOnTextFinishListener(new PasswordEditText.OnTextFinishListener() {
+            @Override
+            public void onFinish(String str) {
+                Log.e(TAG,"onFinish  str= " + str);
+                Toast.makeText(MainActivity.this,"editText3 content= "+str,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/senon/passwordedittext/PasswordEditText.java
@@ -0,0 +1,264 @@
+package com.senon.passwordedittext;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Selection;
+import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * 密码输入框控件
+ */
+public class PasswordEditText extends RelativeLayout {
+
+    private EditText editText; //文本编辑框
+    private Context context;
+
+    private LinearLayout linearLayout; //文本密码的文本
+    private TextView[] textViews; //文本数组
+
+    private int pwdlength = 6; //密码长度， 默认6
+
+    private OnTextFinishListener onTextFinishListener;
+
+
+    public PasswordEditText(Context context) {
+        this(context, null);
+    }
+
+    public PasswordEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PasswordEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+    }
+
+    /**
+     * @param bgDrawable    背景drawable
+     * @param pwdLength     密码长度
+     * @param splilineWidth 分割线宽度  (dp)
+     * @param splilineColor 分割线颜色
+     * @param pwdColor      密码字体颜色
+     * @param pwdSize       密码字体大小 (sp)
+     * @param pwdBg         密码背景
+     */
+    public void initStyle(int bgDrawable, int pwdLength, float splilineWidth, int splilineColor, int pwdColor, int pwdSize, int pwdBg) {
+        this.pwdlength = pwdLength;
+        initEdit(bgDrawable);
+        initShowInput(bgDrawable, pwdlength, splilineWidth, splilineColor, pwdColor, pwdSize, pwdBg);
+    }
+
+    public int getPwdlength() {
+        return pwdlength;
+    }
+
+    public void setPwdlength(int pwdlength) {
+        this.pwdlength = pwdlength;
+    }
+
+    /**
+     * 初始化编辑框
+     * @param bgcolor 输入框背景色
+     */
+    private void initEdit(int bgcolor) {
+        editText = new EditText(context);
+        editText.setBackgroundResource(bgcolor);
+        editText.setCursorVisible(false);
+        editText.setTextSize(0);
+        editText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD | InputType.TYPE_CLASS_NUMBER);
+        editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(pwdlength)});
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Editable etext = editText.getText();
+                Selection.setSelection(etext, etext.length());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                initDatas(s);
+                if (s.length() == pwdlength) {
+                    if (onTextFinishListener != null) {
+                        onTextFinishListener.onFinish(s.toString().trim());
+                    }
+                }
+            }
+        });
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        addView(editText, lp);
+
+    }
+
+    /**
+     * @param bgColor       背景drawable
+     * @param pwdLength     密码长度
+     * @param slpilineWidth 分割线宽度
+     * @param splilineColor 分割线颜色
+     * @param pwdColor      密码字体颜色
+     * @param pwdSize       密码字体大小
+     * @param pwdBg         密码背景
+     */
+    public void initShowInput(int bgColor, int pwdLength, float slpilineWidth, int splilineColor, int pwdColor, int pwdSize, int pwdBg) {
+        //添加密码框父布局
+        linearLayout = new LinearLayout(context);
+        linearLayout.setBackgroundResource(bgColor);
+        LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        linearLayout.setLayoutParams(layoutParams);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        addView(linearLayout);
+
+        //添加密码框
+        textViews = new TextView[pwdLength];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LayoutParams.MATCH_PARENT);
+        params.weight = 1;
+        params.gravity = Gravity.CENTER;
+
+        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(dip2px(context, slpilineWidth), LayoutParams.MATCH_PARENT);
+        for (int i = 0; i < textViews.length; i++) {
+            TextView textView = new TextView(context);
+            textView.setGravity(Gravity.CENTER);
+            textViews[i] = textView;
+            textViews[i].setTextSize(pwdSize);
+            textViews[i].setTextColor(pwdColor);
+            textViews[i].setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD | InputType.TYPE_CLASS_NUMBER);
+            textViews[i].setBackgroundResource(pwdBg);
+            linearLayout.addView(textView, params);
+
+            if (i < textViews.length - 1) {
+                View view = new View(context);
+                view.setBackgroundColor(splilineColor);
+                linearLayout.addView(view, params2);
+            }
+        }
+    }
+
+    /**
+     * 是否显示明文
+     * @param showPwd
+     */
+    public void setShowPwd(boolean showPwd) {
+        int length = textViews.length;
+        for (int i = 0; i < length; i++) {
+            if (!showPwd) {
+                textViews[i].setTransformationMethod(PasswordTransformationMethod.getInstance());
+            } else {
+                textViews[i].setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        }
+    }
+
+    public boolean getShowPwd(){
+        return textViews[0].getTransformationMethod() == HideReturnsTransformationMethod.getInstance();
+    }
+
+    /**
+     * 设置显示类型
+     * @param type
+     */
+    public void setInputType(int type) {
+        int length = textViews.length;
+        for (int i = 0; i < length; i++) {
+            textViews[i].setInputType(type);
+        }
+    }
+
+    /**
+     * 清除文本框
+     */
+    public void clearText() {
+        editText.setText("");
+        for (int i = 0; i < pwdlength; i++) {
+            textViews[i].setText("");
+        }
+    }
+
+    public void setOnTextFinishListener(OnTextFinishListener onTextFinishListener) {
+        this.onTextFinishListener = onTextFinishListener;
+    }
+
+    /**
+     * 根据输入字符，显示密码个数
+     * @param s
+     */
+    public void initDatas(Editable s) {
+        if (s.length() > 0) {
+            int length = s.length();
+            for (int i = 0; i < pwdlength; i++) {
+                if (i < length) {
+                    for (int j = 0; j < length; j++) {
+                        char ch = s.charAt(j);
+                        textViews[j].setText(String.valueOf(ch));
+                    }
+                } else {
+                    textViews[i].setText("");
+                }
+            }
+        } else {
+            for (int i = 0; i < pwdlength; i++) {
+                textViews[i].setText("");
+            }
+        }
+    }
+
+    public String getPwdText() {
+        if (editText != null)
+            return editText.getText().toString().trim();
+        return "";
+    }
+
+    public void setPwdText(String text) {
+        if (editText != null)
+            editText.setText(text);
+    }
+
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public interface OnTextFinishListener {
+        void onFinish(String str);
+    }
+
+    public void setFocus() {
+        editText.requestFocus();
+        editText.setFocusable(true);
+        showKeyBord(editText);
+    }
+
+    /**
+     * 显示键盘
+     * @param view
+     */
+    public void showKeyBord(View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        if (null != editText)
+            editText.setEnabled(enabled);
+    }
+}
